a962fb09bec2347e91ada82a277a162d2ae8c953
shashwat mishra <shashwatmishraog@gmail.com>
2023-06-01 10:23:46 +0530

    s

---
+++ react-techystar-master/src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>First_react_project
+        </h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://youtu.be/7NcDGt3xO4U" target={"blank"}>
+            Youtube
+          </a>
+          <a href=" https://www.instagram.com/shashwatmishraog/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/shashwatmishraog" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
